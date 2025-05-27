2121dcf2779b6eb1b61aa198a631f31a59d660ad
Alexey Bondarev <6933666@mail.ru>
2025-05-27 17:44:23 +0300

    Implementation of the project on Angular with all tasks

---
+++ src/app/components/pages/product/product.component.ts
@@ -0,0 +1,66 @@
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {ProductType} from "../../../types/product.type";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {ProductsService} from "../../../services/products.service";
+import {Subscription} from "rxjs";
+
+
+@Component({
+  selector: 'product-component',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit, OnDestroy {
+  @Input() id: number | undefined;
+  private products: ProductType[] = [];
+  product: ProductType;
+  subscriptionProducts: Subscription | null = null;
+  subscriptionParams: Subscription | null = null;
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private productsService: ProductsService,
+              private router: Router) {
+
+    this.product = {
+      id: 0,
+      image: '',
+      title: '',
+      price: 0,
+      description: ''
+    }
+  }
+
+  ngOnInit(): void {
+    this.subscriptionParams = this.activatedRoute.params
+      .subscribe({
+        next: (params: Params) => {
+          let id = +params['id'];
+          this.subscriptionProducts = this.productsService.getProducts()
+            .subscribe({
+              next: (data) => {
+                let productItem = data.find((product) => {
+                  return product.id === id;
+                })
+                if(productItem) {
+                  this.product.image = productItem.image;
+                  this.product.title = productItem.title;
+                  this.product.price = productItem.price
+                  this.product.description = productItem.description
+                }
+              },
+              error: (error) => {
+                console.log(error)
+              }
+            })
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      })
+  }
+
+  ngOnDestroy() {
+    this.subscriptionParams?.unsubscribe();
+    this.subscriptionProducts?.unsubscribe();
+  }
+}
